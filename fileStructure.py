1d20976a25457e3cf25b2027d452fea8f91c888b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2022-04-12 16:53:08 +0900

    fileStructure.py

--- fileStructure.py
+++ fileStructure.py
@@ -1,16 +1,93 @@
 from pathlib import Path
-import struct
-from ctypes import c_uint32, c_uint16, c_longlong, c_double
-from ctypes import sizeof
-from pprint import pprint
+import ctypes
 
-from objc_util import *
+import struct
 
 SIMToolkitNegativeACK_path_str = '/System/Library/Audio/UISounds/SIMToolkitNegativeACK.caf'
 SIMToolkitNegativeACK_path = Path(SIMToolkitNegativeACK_path_str)
 
 sound_bytes = SIMToolkitNegativeACK_path.read_bytes()
 
+
+class CAFFileHeader(ctypes.Structure):
+  _fields_ = [
+    ('mFileType', ctypes.c_uint32),
+    ('mFileVersion', ctypes.c_uint16),
+    ('mFileFlags', ctypes.c_uint16),
+  ]
+
+
+class CAFChunkHeader(ctypes.Structure):
+  _fields_ = [
+    ('mChunkType', ctypes.c_uint32),
+    ('mChunkSize', ctypes.c_int64),
+  ]
+
+
+class CAFAudioFormat(ctypes.Structure):
+  _fields_ = [
+    ('mSampleRate', ctypes.c_double),
+    ('mFormatID', ctypes.c_uint32),
+    ('mFormatFlags', ctypes.c_uint32),
+    ('mBytesPerPacket', ctypes.c_uint32),
+    ('mFramesPerPacket', ctypes.c_uint32),
+    ('mChannelsPerFrame', ctypes.c_uint32),
+    ('mBitsPerChannel', ctypes.c_uint32),
+  ]
+
+
+f = ctypes.sizeof(CAFFileHeader)
+c = ctypes.sizeof(CAFChunkHeader)
+a = ctypes.sizeof(CAFAudioFormat)
+
+print(f, c, a)
+h = f + c + a
+
+structs = sound_bytes[:h]
+
+#cafFileHeader = structs[:f]
+#cafChunkHeader = structs[f:f + c]
+#cafAudioFormat = structs[f + c:f + c + a]
+#print(cafFileHeader, ':CAFFileHeader')
+#print(cafChunkHeader, ':CAFChunkHeader')
+#print(cafAudioFormat, ':CAFAudioFormat')
+
+#print(ctypes.sizeof(ctypes.c_uint16), 'uint16')
+#print(ctypes.sizeof(ctypes.c_uint32), 'uint32')
+#print(ctypes.sizeof(ctypes.c_int64), 'int64')
+print(ctypes.sizeof(ctypes.c_double), 'double')
+
+#mChunkSize = b'\x00\x00\x00\x00\x00\x00\x00 @\xd5\x88\x80'
+
+UInt16 = ctypes.sizeof(ctypes.c_uint16)
+UInt32 = ctypes.sizeof(ctypes.c_uint32)
+SInt64 = ctypes.sizeof(ctypes.c_int64)
+Float64 = ctypes.sizeof(ctypes.c_double)
+
+ff = UInt32 + UInt16 + UInt16
+cc = UInt32 + SInt64
+aa = Float64 + UInt32 + UInt32 + UInt32 + UInt32 + UInt32 + UInt32
+
+cafFileHeader = structs[:ff]
+cafChunkHeader = structs[ff:ff + cc]
+cafAudioFormat = structs[ff + cc:ff + cc + aa]
+
+
+print(cafFileHeader, ':CAFFileHeader')
+print(cafChunkHeader, ':CAFChunkHeader')
+print(cafAudioFormat, ':CAFAudioFormat')
+mChunkType = cafChunkHeader[:UInt32]
+mChunkSize = cafChunkHeader[UInt32:]
+mSampleRate = cafAudioFormat[:Float64]
+
+#longlong = struct.pack('q', 1)
+
+#print(ctypes.c_uint32(caf))
+
+print(struct.unpack('q', mChunkSize), ':mChunkSize')
+print(struct.unpack('d', mSampleRate), ':mSampleRate')
+#print(ctypes.sizeof(ctypes.c_longlong))
+'''
 s = 0
 e = sizeof(c_uint32(0))
 mFileType = sound_bytes[s:e]
@@ -47,3 +124,6 @@ mFormatFlags = sound_bytes[s:e]
 
 sample = 44800.0
 print(struct.pack('<f', sample))
+
+'''
+
--- translatedDocument/Core_Audio_Format_Specification.md
+++ translatedDocument/Core_Audio_Format_Specification.md
@@ -1,3 +1,64 @@
 [Core Audio Format Specification](https://developer.apple.com/library/archive/documentation/MusicAudio/Reference/CAFSpec/CAF_spec/CAF_spec.html#//apple_ref/doc/uid/TP40001862-CH210-SW1)
 
+この章では、AppleのCoreAudioFormatについて説明および指定します。  CAFの機能やファイルのレイアウトなど、CAFの概要については、CAFファイルの概要を参照してください。
 
+> **重要：** このドキュメントでは、標準のC構造体と列挙型の宣言を使用して、CAFファイルヘッダーとCAFチャンクの詳細を指定します。 これは表記上の便宜です。  CAFファイルのデータは、Cコンパイラで解析できず、実際のC構造体または列挙体を構成しません。 たとえば、CAFファイルには、正しいバイトアラインメントを保証するためのパッドフィールドはありません。  Cからのもう1つの違いは、「構造体」内の複数の「フィールド」の長さが異なる可能性があることです。
+> 一方、このドキュメントに含まれているものと同様のC構造体を使用して、CAFファイルから解析されたデータを保持できます。 この仕様で使用される構造名（CAFAudioFormatなど）とフィールド名（mChunkSizeなど）は任意ですが、それらの多くはAudioToolbox/CAFFile.hで使用される名前に対応しています。
+
+
+# データ型
+
+> Data Types
+
+CAFファイルのすべてのフィールドはビッグエンディアン（ネットワーク）バイトオーダーですが、オーディオデータは例外で、データ形式に応じてビッグエンディアンまたはリトルエンディアンになります。 音声データの形式は、音声ガイドチャンクによって記述されます。
+
+CAFファイルのすべての浮動小数点フィールドは、IEEE-754仕様に準拠している必要があります。  http://grouper.ieee.org/groups/754/を参照してください。
+
+
+
+# CAFファイルヘッダーとチャンクヘッダー
+> CAF File Header and Chunk Headers
+
+CAFファイルヘッダーと各チャンクのチャンクヘッダーは、すべてのCAFファイルの必須要素です。 それらは、ファイルとそのチャンクを自己記述的にするのに役立ちます。
+
+
+## CAFファイルヘッダー
+> CAF File Header
+
+CAFファイルは単純なヘッダーで始まります。  `CAFFileHeader`構造体は、ファイルヘッダーを記述します。
+
+
+``` CAFFileHeader.c
+struct CAFFileHeader {
+    UInt32  mFileType;
+    UInt16  mFileVersion;
+    UInt16  mFileFlags;
+};
+```
+
+### mFileType
+
+ファイルの種類。 この値は`caff` に設定する必要があります。  `mFileType` フィールドが`caff`に設定されているファイルのみを有効なCAFファイルと見なす必要があります。
+
+
+### mFileVersion
+
+ファイルバージョン。 この仕様に準拠するCAFファイルの場合、バージョンを`1` に設定する必要があります。Appleがこの仕様の大幅なリビジョンをリリースする場合、そのリビジョンに準拠するファイルの`mFileVersion` フィールドは`1` より大きい数値に設定されます。
+
+
+### mFileFlags
+
+将来の使用のためにAppleによって予約されたフラグ。  CAF v1ファイルの場合、`0` に設定する必要があります。理解できないこのフィールドの値は無視し、バージョンとファイルタイプのフィールドが有効である限り、ファイルを有効なCAFファイルとして受け入れる必要があります。
+
+
+## CAFチャンクヘッダー
+> CAF Chunk Header
+
+CAFファイルのすべてのチャンクにはヘッダーがあり、そのような各ヘッダーには、`CAFChunkHeader` 構造に示されているように2つの必須フィールドが含まれています。
+
+
+```  CAFChunkHeader.c
+struct CAFChunkHeader {
+    UInt32  mChunkType;
+    SInt64  mChunkSize;
+};
